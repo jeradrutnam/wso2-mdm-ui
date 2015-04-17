295d22a61a1fb416fa27fc72a35ff7d976b6a142
Jerad Rutnam <jeradrutnam@outlook.com>
2015-04-17 11:26:37 +0530

    Base colors revamp & ui fixes/updates

--- js/assetmenu.js
+++ js/assetmenu.js
@@ -93,9 +93,16 @@ function containerUpdate(asset){
         +'onclick="selectAsset(this)"'
         +'asset="' + $(asset).attr('asset') + '">'+ $(asset).html() +'</li>'
         +'<a id="goBack" href="javascript:void(0);" onclick="goBack(this); return false;" '
-        +'class="cu-btn" title="Go Back"><i class="wso2icon wso2-c-left-arrow"></i></a>'
+        +'class="cu-btn" title="Go Back">'
+        +'<span class="fw-stack">'
+        +'<i class="fw fw-ring fw-stack-2x"></i>'
+        +'<i class="fw fw-left-arrow fw-stack-1x"></i>'
+        +'</span></a>'
         +'<a id="resetNav" href="javascript:void(0);" onclick="resetNav(); return false;" '
-        +'class="cu-btn" title="Reset"><i class="wso2icon wso2-refresh"></i></a>'
+        +'class="cu-btn" title="Reset">'
+        +'<span class="fw-stack">'
+        +'<i class="fw fw-refresh fw-stack-2x"></i>'
+        +'</span></a>'
         +'</ul>' + '<ul class="options">'+ options +'</ul>');
 
     prevSelected = ($(menuSubContainer + ' ul.selected li').attr('asset'));
